d089a517d68203285105c5385fc18930d1196d10
777388 <96343159+777388@users.noreply.github.com>
2023-01-09 17:54:36 -0800

    Update topsecret.py

--- topsecret.py
+++ topsecret.py
@@ -13,23 +13,27 @@ def taskse():
             for k in range(255):
                 for d in range(255):
                     thing = requests.get("http://web.archive.org/cdx/search/cdx?url="+str(i)+"."+str(n)+"."+str(k)+"."+str(d)+"&output=text&fl=original&collapse=urlkey&matchType=prefix", verify=False)
+                    time.sleep(4)
                     stri = "stri.txt"
                     f = open(stri, 'w')
                     print(thing.text.strip(), file=f)
                     fil = open(stri, 'r')
                     for line in fil:
                         thong = requests.get("http://webcache.googleusercontent.com/search?q=cache:"+line, verify=False)
-                        time.sleep(4)
+                        time.sleep(1)
                         if thong:
                             buffer = open('buffer.txt', 'w')
                             print(thong.text.strip(), file=buffer)
                             buffed = open('buffer.txt', 'r')
                             for liner in buffed:
-                                theng = os.popen("grep "+rer+" buffer.txt")
-                                if theng:
-                                    print("http://webcache.googleusercontent.com/search?q=cache:"+liner)
-                                else:
+                                thung = os.popen("grep 404 buffer.txt")
+                                
+                                if thung:
                                     break
+                                else:
+                                    theng = os.popen("grep "+rer+" buffer.txt")
+                                    if theng:
+                                        print("http://webcache.googleusercontent.com/search?q=cache:"+liner)
                         
 
 if __name__ == '__main__':
