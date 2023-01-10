2e2b28af687e26acdecfdab300bc5473eaae9aaa
777388 <96343159+777388@users.noreply.github.com>
2023-01-09 18:57:04 -0800

    Update natotopsecret.py

--- natotopsecret.py
+++ natotopsecret.py
@@ -3,32 +3,42 @@ from multiprocessing import Process
 import sys
 import requests
 import time
+import urllib3
+urllib3.disable_warnings()
 print("usage python3 natotopsecret.py searchterm domain")
 rer = sys.argv[1]
 rawr = sys.argv[2]
 
 def taskse():
     thing = requests.get("http://web.archive.org/cdx/search/cdx?url="+rawr+"&output=text&fl=original&collapse=urlkey&matchType=prefix", verify=False)
-    stri = "stri.txt"
-    fin = open(stri, 'w')
-    print(thing.text.strip(), file=fin)
-    fil = open(stri, 'r')
-    for line in fil:
-        thong = requests.get("http://webcache.googleusercontent.com/search?q=cache:"+line, verify=False)
-        time.sleep(1)
-        if thong:
-            buffer = open('buffer.txt', 'w')
-            print(thong.text.strip(), file=buffer)
-            buffed = open('buffer.txt', 'r')
-            for liner in buffed:
-                thung = os.popen("grep 404 buffer.txt")
-                if thung:
-                    break
-                else:
-                    theng = os.popen("grep "+rer+" buffer.txt")
-                    if theng:
-                        print("http://webcache.googleusercontent.com/search?q=cache:"+liner)
-                        
-
+    if thing.ok:
+        stri = "stri.txt"
+        fin = open(stri, 'w')
+        print(thing.text.strip(), file=fin)
+        fil = open(stri, 'r')
+        for line in fil:
+            thong = requests.get("https://cc.bingj.com/cache.aspx?q="+line, verify=False)
+            time.sleep(4)
+            if thong.ok:
+                buffer = open('buffer.txt', 'w')
+                print(thong.text.strip(), file=buffer)
+                buffed = open('buffer.txt', 'r')
+                for liner in buffed:
+                    thung = os.popen("grep -e Apologies buffer.txt")
+                    if thung == 1:
+                        theng = os.popen("grep -e "+rer+" buffer.txt")
+                        if theng != 1:
+                            print("http://webcache.googleusercontent.com/search?q=cache:"+liner)
+                        else:
+                            continue
+                    else:
+                        continue
+            else:
+                print("too many requests sent to bing, please wait 5 minutes")
+                return 5
+    else:
+        print("too many requests sent to web.archive.org, please wait 5 minutes")
+        return 5                
+        
 if __name__ == '__main__':
     taskse()
