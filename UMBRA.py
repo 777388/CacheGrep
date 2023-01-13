3b950e93bf0c9e922e9d164a6739d33bcb71831c
777388 <96343159+777388@users.noreply.github.com>
2023-01-12 17:18:00 -0800

    Update UMBRA.py

--- UMBRA.py
+++ UMBRA.py
@@ -12,7 +12,7 @@ import os
 from itertools import islice
 from multiprocessing import Process
 from multiprocessing import Pool
-print("usage python3 UMBRA.py searchterm outputfile")
+print("usage python3 UMBRA.py searchterm file")
 rer = sys.argv[1]
 rawr = sys.argv[2]
 yup = str(rawr)
@@ -36,11 +36,11 @@ def proxiess():
 def thumbs(lotty):
     try:
         while True: 
-            #try:
-            r = requests.head("http://webcache.googleusercontent.com/search?q=cache:"+urllib.parse.quote_plus(lotty), allow_redirects=True, proxies=proxiess())
+            try:
+                r = requests.head("http://webcache.googleusercontent.com/search?q=cache:"+urllib.parse.quote_plus(lotty), allow_redirects=True, proxies=proxiess())
             #time.sleep(0.1)
-            #except requests.exceptions.ProxyError as err:
-                #print("Proxy Error", err)
+            except Exceptions as e:
+                print("\rProxy Error"+e, end="")
             buffet = open('buffet.txt', 'w')
             print(r.url, file=buffet)
             buffet.close()
@@ -49,11 +49,11 @@ def thumbs(lotty):
             if subprocess.Popen("grep -e sorry buffet.txt", stdout=subprocess.PIPE, shell=True).communicate():
                 buffete.close()
                 print("\rredirected to captcha, avoiding", end="")
-                #try:
-                thumb = requests.get("http://webcache.googleusercontent.com/search?q=cache:"+urllib.parse.quote_plus(lotty), allow_redirects=False, proxies=proxiess())
+                try:
+                    thumb = requests.get("http://webcache.googleusercontent.com/search?q=cache:"+urllib.parse.quote_plus(lotty), allow_redirects=False, proxies=proxiess())
                 #time.sleep(0.1)
-                #except requests.exceptions.ProxyError as err:
-                #print("Proxy Error", err)
+                except Exceptions as e:
+                    print("\rProxy Error"+e, end="")
                 if thumb.ok:
                     buffage = open('buffage.txt', 'w')
                     print(thumb.text, file=buffage)
@@ -61,14 +61,14 @@ def thumbs(lotty):
                     buffage.close()
                     buffaged = open('buffage.txt', 'r')
                     thump = subprocess.Popen("grep -e 404 buffage.txt", stdout=subprocess.PIPE, shell=True).communicate()
-                    if thump:
+                    if thump.stdout.readline().decode('utf-8'):
                         print("\rsaw 404", end='')
                         buffaged.close()
                         return 5
                     else:
                         thenn = subprocess.Popen("grep -e "+rer+" buffage.txt", stdout=subprocess.PIPE, shell=True).communicate()
                         buffaged.close()
-                        if thenn:
+                        if thenn.stdout.readline().decode('utf-8'):
                             print("http://webcache.googleusercontent.com/search?q=cache:"+urllib.parse.quote_plus(lotty))
                             print("http://webcache.googleusercontent.com/search?q=cache:"+urllib.parse.quote_plus(lotty), file=wude)
                             return 6
@@ -80,11 +80,10 @@ def thumbs(lotty):
                     return 8
             elif subprocess.Popen("grep -e google buffet.txt", stdout=subprocess.PIPE, shell=True).communicate():
                 buffete.close()
-                #try:
-                thumb = requests.get("http://webcache.googleusercontent.com/search?q=cache:"+urllib.parse.quote_plus(lotty), allow_redirects=True, proxies=proxiess())
-                thumb.raise_for_status()
-                #except requests.exceptions.ProxyError as err:
-                    #print("Proxy Error", err)
+                try:
+                    thumb = requests.get("http://webcache.googleusercontent.com/search?q=cache:"+urllib.parse.quote_plus(lotty), allow_redirects=True, proxies=proxiess())
+                except Exceptions as e:
+                    print("\rProxy Error"+e, end="")
                 if thumb.ok:
                     buffage = open('buffage.txt', 'w')
                     print(thumb.text, file=buffage)
@@ -92,14 +91,14 @@ def thumbs(lotty):
                     buffage.close()
                     buffaged = open('buffage.txt', 'r')
                     thump = subprocess.Popen("grep -e 404 buffage.txt", stdout=subprocess.PIPE, shell=True).communicate()
-                    if thump:
+                    if thump.stdout.readline().decode('utf-8'):
                         print("\rsaw 404", end='')
                         buffaged.close()
                         return 5
                     else:
                         thenn = subprocess.Popen("grep -e "+rer+" buffage.txt", stdout=subprocess.PIPE, shell=True).communicate()
                         buffaged.close()
-                        if thenn:
+                        if thenn.stdout.readline().decode('utf-8'):
                             print("http://webcache.googleusercontent.com/search?q=cache:"+urllib.parse.quote_plus(lotty))
                             print("http://webcache.googleusercontent.com/search?q=cache:"+urllib.parse.quote_plus(lotty), file=wude)
                             return 6
@@ -118,31 +117,31 @@ def thumbs(lotty):
 def thongs(litty):
     try:
         while True:
-            #try:
-            thong = requests.get("https://cc.bingj.com/cache.aspx?q="+litty.strip(), allow_redirects=False, proxies=proxiess())
+            try:
+                thong = requests.get("https://cc.bingj.com/cache.aspx?q="+litty.strip(), allow_redirects=False, proxies=proxiess())
             #time.sleep(0.1)
-            thong.raise_for_status()
-            #except requests.exceptions.ProxyError as err: 
-                #print("proxy error", err)
+            except requests.exceptions.ProxyError as err: 
+                print("\rproxy error"+e, end="")
             if thong.ok:
                 buffer = open('buffer.txt', 'w')
                 print(thong.text.strip(), file=buffer)
+                thong.raise_for_status()
                 buffer.close()
                 #buffed = open('buffer.txt', 'r')
                 thung = subprocess.Popen("grep -e Apologies buffer.txt", stdout=subprocess.PIPE, shell=True).communicate()
-                if thung:
+                if thung.stdout.readline().decode('utf-8'):
                     #buffed.close()
                     print("\rsaw apologies, cache in "+litty.strip()+" not found", end='')
                     return 5
                 else:
                     theng = subprocess.Popen("grep -e "+rer+" buffer.txt", stdout=subprocess.PIPE, shell=True).communicate() 
                     #buffed.close()
-                    if theng:
+                    if theng.stdout.readline().decode('utf-8'):
                         print("https://cc.bingj.com/cache.aspx?q="+litty) 
                         print("https://cc.bingj.com/cache.aspx?q="+litty, file=wude)
                         return 6
                     else:
-                        print("\rdid not find cache", end='')
+                        print("\rdid not find "+rer+" in cache", end='')
                         return 7    
             else:
                 print("\rtoo many requests sent to bing, please wait 5 minutes", end='')
@@ -162,16 +161,17 @@ def taskse():
                         print(thing.text.strip(), file=fin)
                         fil = open(stri, 'r')
                         try:
-                            processes =[Process(target=thongs, args=(lineee,)) for lineee in fil]
+                            
                             processess =[Process(target=thumbs, args=(lineee,)) for lineee in fil]
-                            for process in processes:
+                            processes =[Process(target=thongs, args=(lineee,)) for lineee in fil]
+                            for process in processess:
                                 process.start()
-                            for processs in processess:
+                            for processs in processes:
                                 processs.start()
-                            for process in processes:
-                                process.join()
                             for process in processess:
                                 process.join()
+                            for processs in processes:
+                                processs.join()
                         except Exception as e:
                             print("well, your computer thinks thats too many processes, try a smaller file")
                             print(e)
